a590b058c77ef60f1035ca1821d10fb6d6f1f52b
p0k0 <m11e@ya.ru>
2020-04-04 08:40:03 +0300

    add event.proj

---
+++ app.immutable/event/IUserUpdated.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using app.immutable.data;
+
+namespace app.immutable.@event
+{
+    public interface IUserUpdated
+    {
+        public IAppeal Appeal { get; }
+        public ICommunicationSource CommunicationSource { get; }
+    }
+}
